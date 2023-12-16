c1660845b30db4f11ee3dae93543085e49026593
AsHiK-M(C.D) <ashik018361@gmaill.com>
2023-12-16 16:33:55 +0600

    crear a new file server site with mongodb

--- car-doctors-clint/src/Pages/Shared/NavBar/NavBar.jsx
+++ car-doctors-clint/src/Pages/Shared/NavBar/NavBar.jsx
@@ -24,11 +24,13 @@ const NavBar = () => {
     // html code with dainamic
     const naveItems = <>
 
-        <li className=' text-green-500'>
-            {
+        <li className=' text-green-500 mt-2'>
+          
+           {
                 user &&
                 user.email
             }
+           
         </li>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
@@ -46,25 +48,29 @@ const NavBar = () => {
     </>
     return (
         <div className="navbar bg-base-100 h-20 mt-5 mb-4">
+            <Link to="/" className="mt-3"><img src={logo} alt="" /></Link>
             <div className="navbar-start">
-                <div className="dropdown">
+
+
+                <div className="dropdown relative">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content ms-1 p-2 shadow bg-base-100 rounded-box w-52 absolute  border">
                         {naveItems}
                     </ul>
                 </div>
-                <Link to="/" className="mt-3"><img src={logo} alt="" /></Link>
+
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+            <div className="navbar-center hidden lg:flex ">
+                <ul className="menu menu-horizontal gap-2">
                     {naveItems}
                 </ul>
             </div>
 
 
-            <div className="navbar-end">
+            <div className="navbar-end gap-3">
+
                 <p>Ashik</p>
 
                 <div className="tooltip" data-tip='Your Name'>
